a2e89a29ae20f63491a688c9408ae0490210e873
AhmedNoaman602 <ahmednoaman002@gmail.com>
2026-06-01 15:51:59 +0300

    added an order date column in the orders table

---
+++ database/migrations/2026_05_31_215017_add_order_date_to_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('orders', function (Blueprint $table) {
+        $table->date('order_date')->default(DB::raw('(DATE(created_at))'))->after('discount');
+    });
+}
+
+public function down(): void
+{
+    Schema::table('orders', function (Blueprint $table) {
+        $table->dropColumn('order_date');
+    });
+}
+};
